6b1f612f9e8e723eecf75659f76d7aa7ab012d8f
Willa <willasimard@gmail.com>
2022-09-29 10:49:25 -0600

    update

--- firstassignment_redo/sketch.js
+++ firstassignment_redo/sketch.js
@@ -5,11 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let startScreen = true;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(0);
+  noStroke();
+  if (startScreen === false) {
+    drawFloor();
+  }
 }
+
+
+
+
+
+
+function drawFloor() {
+  let floorHeight = windowHeight/4;
+  rect(0, windowHeight - floorHeight, windowWidth, floorHeight); 
+}
+
+
