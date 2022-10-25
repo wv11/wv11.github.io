1ab21065520854c4b135a497127ef0ac5022d8e8
Willa <willasimard@gmail.com>
2022-10-25 13:03:08 -0600

    hfrejiuofhneujiwfnh

--- terrain_generation/sketch.js
+++ terrain_generation/sketch.js
@@ -9,10 +9,18 @@ let theHeightsFloor = [];
 let theHeightsBG = [];
 let startingLocationFloor = 0;
 let startingLocationBG = 0;
-let floorMoveSpeed = 15;
-let bgMoveSpeed = 5;
+let floorMoveSpeed = 10;
+let bgMoveSpeed = 2;
 let barrierStart = 35;
 let speedMultiplyer = 2;
+let characterSprite;
+let state;
+
+function preload() {
+  characterSprite = loadImage("oie_25203228iwiUzMjW.gif");
+
+}
+
 
 
 
@@ -20,20 +28,33 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   theHeightsFloor = generateHeightsFloor(10000);
   theHeightsBG = generateHeightsBG(10000);
+  imageMode(CENTER);
 }
 
 function draw() {
+
   let barrierEnd = 10000 - width - 35;
-  background(255);
+  background(77, 136, 255);
+
+  
   //fill(0);
   for (let i = startingLocationBG; i < startingLocationBG + width; i++) {
     
     displayRectangleBG(i-startingLocationBG, theHeightsBG[i], 1);
   }
 
-  for (let i = startingLocationFloor; i < startingLocationFloor + width; i++) {
+
+  for (let i = startingLocationFloor; i < startingLocationFloor + width; i++) { 
     displayRectangleFloor(i-startingLocationFloor, theHeightsFloor[i], 1);
+    
   }
+  for (let i = startingLocationFloor; i < startingLocationFloor + width; i++) {
+    
+    image(characterSprite, width/2, height - theHeightsFloor[i-width/2] - 150, 400, 400);
+  }
+
+
+
   if (keyIsDown(68) && startingLocationFloor <= barrierEnd) {
     startingLocationFloor+=  floorMoveSpeed;
     startingLocationBG += bgMoveSpeed;
@@ -50,19 +71,20 @@ function draw() {
       startingLocationBG -= bgMoveSpeed*speedMultiplyer;
     }
   }
+  
 
 }
 
 function displayRectangleFloor(x, rectHeight, rectWidth) {
-  fill(0);
-  stroke(0);
+  fill(0, 26, 77);
+  stroke(0, 26, 77);
   let y = height - rectHeight;
   rect(x, y, rectWidth, rectHeight);   
 }
 
 function displayRectangleBG(x, rectHeight, rectWidth) {
-  fill(150);
-  stroke(150);
+  fill(0, 60, 179);
+  stroke(0, 60, 179);
   let y = height - rectHeight;
   rect(x, y, rectWidth, rectHeight);
 }
@@ -72,8 +94,8 @@ function generateHeightsFloor(howMany) {
   let tempArray = [];
   let time1 = random(10000);
   for (let i = 0; i < howMany; i++) {
-    tempArray.push(noise(time1) * height/2.4);
-    time1 += 0.0009;
+    tempArray.push(noise(time1) * height/3);
+    time1 += 0.0007;
 
   }
   return tempArray;
@@ -84,9 +106,10 @@ function generateHeightsBG(howMany) {
   let newArray = [];
   let time2 = random(10000);
   for (let i = 0; i < howMany; i++) {
-    newArray.push(noise(time2) * height);
+    newArray.push(noise(time2) * height*1.1);
     time2 += 0.001;
   }
   return newArray;
 }
+
 
