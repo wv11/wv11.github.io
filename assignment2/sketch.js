23695ab627bf8e41b8d1cf0b1ab6b645bd428291
Willa <willasimard@gmail.com>
2022-10-28 12:16:58 -0600

    Update sketch.js

--- assignment2/sketch.js
+++ assignment2/sketch.js
@@ -5,6 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rows = 10;
+let cols = 10;
+let base;
+let cellHeight;
+let cellWidth;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
