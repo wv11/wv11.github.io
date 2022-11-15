e6f126af6d5ce8cbf28e158547bd42e06421bb9e
Willa <willasimard@gmail.com>
2022-11-15 12:15:29 -0600

    mbmjmhmj

---
+++ oop/sketch.js
@@ -0,0 +1,69 @@
+// Walker OOP Demo
+
+class Walker {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = "red";
+    this.speed = 5;
+    this.radius = 1;
+  }
+
+  display() {
+    stroke(this.color);
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
+  }
+
+  move() {
+    let choice = random(100);
+
+    if (choice < 25) {
+      //up
+      this.y -= this.speed;
+    }
+    else if (choice < 50) {
+      //down
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      //right
+      this.x += this.speed;
+    }
+    else {
+      //left
+      this.x -= this.speed;
+    }
+
+  }
+}
+
+
+
+let walkerArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  spawnWalker();
+
+
+}
+
+function draw() {
+  for (let i = 0; i < walkerArray.length; i++) {
+    walkerArray[i].move();
+    walkerArray[i].display();
+  }
+}
+
+function spawnWalker() {
+  let walker = new Walker(random(width), random(height));
+  let someColor = color(random(255), random(255), random(255));
+  walker.color = someColor;
+  walkerArray.push(walker);
+
+}
+
+function keyPressed() {
+  spawnWalker();
+}
